7df6375152afc4c76ba5930b74a993f1a4729ca0
Michal99Zajac <michal.99.zajac@gmail.com>
2022-04-10 17:15:50 +0200

    refactor(User): give up in props and hooks dir

--- src/users/models/User/index.ts
+++ src/users/models/User/index.ts
@@ -6,13 +6,21 @@ import {
   PropType,
   Ref,
 } from '@typegoose/typegoose'
+import validator from 'validator'
+import { v4 } from 'uuid'
+import bcrypt from 'bcrypt'
 
+import { config } from '../../../config'
 import { RoleClass } from '../Role'
 
-import { props } from './props'
-import { save } from './hooks'
+const { SALT_ROUNDS } = config
 
-@pre('save', save)
+@pre<UserClass>('save', async function save() {
+  const hash = await bcrypt.hash(this.password, SALT_ROUNDS)
+
+  this.id = v4()
+  this.password = hash
+})
 @modelOptions({
   schemaOptions: {
     collection: 'Users',
@@ -20,22 +28,52 @@ import { save } from './hooks'
   },
 })
 export class UserClass {
-  @prop(props['id'])
+  @prop({
+    type: () => String,
+    unique: true,
+    immutable: true,
+  })
   public id: string
 
-  @prop(props['name'])
+  @prop({
+    type: () => String,
+    required: [true, 'Name is required'],
+    maxlength: [255, 'Name is too long'],
+  })
   public name: string
 
-  @prop(props['lastname'])
+  @prop({
+    type: () => String,
+    required: [true, 'Lastname is required'],
+    maxlength: [255, 'Lastname is too long'],
+  })
   public lastname: string
 
-  @prop(props['email'])
+  @prop({
+    type: () => String,
+    required: [true, 'Email is required'],
+    unique: true,
+    validate: {
+      validator: (email: string) => validator.isEmail(email),
+      message: (value) => `${value.value} is not an email`,
+    },
+  })
   public email: string
 
-  @prop(props['src'])
+  @prop({
+    type: () => String,
+    default: null,
+  })
   public src: string
 
-  @prop(props['password'])
+  @prop({
+    type: () => String,
+    required: [true, 'Password is required'],
+    validate: {
+      validator: (password: string) => validator.isStrongPassword(password),
+      message: 'Password is too weak',
+    },
+  })
   public password: string
 
   @prop(
