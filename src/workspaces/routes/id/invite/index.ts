4201b0a0481ec5d0fea99f6fb8596dd129a6d12f
Michal99Zajac <michal.99.zajac@gmail.com>
2022-05-08 12:48:59 +0200

    feat(workspace): add basic of invite users endpoint

--- src/workspaces/routes/id/invite/index.ts
+++ src/workspaces/routes/id/invite/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Router } from 'express'
 import { BadRequest } from 'http-errors'
 
@@ -8,6 +9,49 @@ import { isWorkspaceAuthor, canCatchWorkspace } from '@workspaces/middlewares'
 import { prettyError } from '@common/utils'
 
 export const router = Router()
+
+router.get(
+  '/workspaces/:workspaceId/invite',
+  canCatchWorkspace,
+  isWorkspaceAuthor,
+  async (req, res, next) => {
+    const workspace = res.locals.workspace as Workspace
+    const user = req.user as User
+    const query = {
+      limit: isNaN(+req.query.limit) ? 0 : +req.query.limit,
+      page: isNaN(+req.query.page) ? 0 : +req.query.page,
+      query: req.query.query,
+    }
+
+    try {
+      const userQuery = UserModel.find({
+        $not: {
+          $eq: user._id,
+        },
+      }).getQuery()
+
+      const count = await UserModel.find(userQuery).countDocuments()
+      const users = await UserModel.find(userQuery)
+        .skip(query.page * query.limit)
+        .limit(query.limit)
+        .sort('name')
+        .transform((users) => users.map((user) => user.public))
+
+      res.json({
+        users: users,
+        navigation: {
+          first: 0,
+          next: query.page + 1,
+          previous: query.page - 1,
+          last: count % query.limit,
+          count: count,
+        },
+      })
+    } catch (error) {
+      next(new BadRequest(prettyError(error)))
+    }
+  }
+)
 
 router.post(
   '/workspaces/:workspaceId/invite',
